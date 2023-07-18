1b42025eeac5e948c47ce5de32e2a79d8117392f
Tehnoking <ashispandey138cgmail.com>
2023-07-18 22:16:54 +0530

    anii dall hai

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
+import Header from './components/Header';
 import{BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import Home from './components/Home';
 function App() {
   return (
     <Router>
       <Header/>
       <Routes>
-        <Route/>
+        <Route path="/" element={<Home/>}/>
       </Routes>
     </Router>
    
